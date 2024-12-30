fd155e637e512a593ed6fd515b5eb076d3e96950
Litonuddinnil <mdniloyhasan544@gmail.com>
2024-12-30 15:52:40 +0600

    complete custom hook and some modify

--- src/CustomHook/useAxiosSecure.jsx
+++ src/CustomHook/useAxiosSecure.jsx
@@ -1,4 +1,7 @@
 import axios from 'axios';
+import { useEffect } from 'react';
+import useAuth from './useAuth';
+import { useNavigate } from 'react-router-dom';
 // import React from 'react';
 
 const axiosInstance = axios.create({
@@ -6,13 +9,22 @@ const axiosInstance = axios.create({
     withCredentials:true
 })
 const useAxiosSecure = () => {
+    const {logOut} =useAuth();
+    const navigate = useNavigate();
     useEffect(()=>{
         axiosInstance.interceptors.response.use(response =>{
             return response;
         },error =>{
-            console.log("eroor")
+            console.log("error")
             if(error.status === 401 || error.status === 403){
-                console.log("need to logout")
+                logOut()
+                .then(()=>{
+                    console.log("logged out")
+                    navigate("/login")
+                })
+                .catch((error)=>{
+                    console.log(error.message);
+                })
             }
             return Promise.reject(error);
         })
